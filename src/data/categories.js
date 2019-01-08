42933375e502ef9e3d62b873e508c54fe1c1ffff
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-01-08 20:01:39 +0200

    add data files

---
+++ src/data/categories.js
@@ -0,0 +1,47 @@
+import javascript from '../images/javascript-group.svg'
+import react from '../images/react-group.svg'
+import html from '../images/html-group.svg'
+import continuous from '../images/continuous-group.svg'
+
+export default [
+  {
+    title: 'React.js Training',
+    slug: 'react-training',
+    description: `Since its open-source release by Facebook in 2013, the React library has exploded in popularity due to its performance features and declarative style. Many related tools and frameworks have emerged to supplement the features of React, and our classes introduce these with practicality in mind. 
+    
+Our React training goes beyond the basics to explore testing with Jest, routing with React Router, and universal apps with Redux. We also go beyond the browser with our React Native course.`,
+    thumb: `${react}`,
+    button: 'View More **→**',
+    tags: '',
+  },
+  {
+    title: 'JavaScript & Node.js Training',
+    slug: 'javascript-and-node-training',
+    description: `With JavaScript and Node.js training from Moon Highway, your team will learn the skills necessary to be successful at building full-stack JavaScript applications. Our beginner JavaScript courses emphasize the fundamentals of JavaScript. 
+    
+They are designed for developers, engineers, creatives and managers who have very little JavaScript experience. Our Node.js materials are designed to train you for the job. We focus heavily on unit testing, best practices, and real world scenarios.`,
+    thumb: `${javascript}`,
+    button: 'View More **→**',
+    tags: '',
+  },
+  {
+    title: 'HTML & CSS Training',
+    slug: 'html-and-css-training',
+    description: `Make your pages semantic, responsive, and accessible with the latest HTML and CSS techniques. 
+    
+Whether you're creating elements for a webpage or styling components in your app, using the most efficient HTML and CSS will help showcase your content in the best way possible.`,
+    thumb: `${html}`,
+    button: 'View More **→**',
+    tags: '',
+  },
+  {
+    title: 'Continuous Delivery',
+    slug: 'continuous-delivery',
+    description: `Continuous Delivery is the practice of deploying your code to production without any human intervention.It allows your business products to rapidly incorporate feedback and instantly push new features to production without compromising quality. 
+    
+The JavaScript community has plenty of tools that we can use to build continuous delivery systems.Our courses are geared towards students who want to master the patterns and tools associated with JavaScript build pipelines.`,
+    thumb: `${continuous}`,
+    button: 'View More **→**',
+    tags: '',
+  },
+]
