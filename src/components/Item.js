20ed2816aa2344585f360846514e4aa2254b3fb1
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-01-09 18:52:36 +0200

    minor fixes to Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,6 +5,127 @@ import { css } from '@emotion/core'
 import { bpMaxSM, bpMaxMD } from '../lib/breakpoints'
 import MDReactComponent from 'markdown-react-js'
 
+const ItemInfo = styled.div`
+  width: 100%;
+  opacity: 0.9;
+  font-size: 17px;
+  color: #090909;
+  letter-spacing: 0;
+  line-height: 26px;
+  ${bpMaxSM} {
+    width: auto;
+  }
+`
+
+const ItemBox = styled.div`
+  & + & {
+    margin-top: 100px;
+  }
+  display: flex;
+  align-items: center;
+  ${bpMaxMD} {
+    align-items: flex-start;
+  }
+  ${bpMaxSM} {
+    align-items: center;
+  }
+  justify-content: center;
+  flex-direction: row;
+  width: 100%;
+  max-width: 920px;
+  :nth-of-type(odd) {
+    flex-direction: row-reverse;
+    .info {
+      padding-left: 0px;
+      padding-right: 80px;
+      ${bpMaxMD} {
+        padding-right: 40px;
+      }
+    }
+  }
+  .info {
+    padding-left: 80px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    ${bpMaxMD} {
+      padding-left: 40px;
+    }
+  }
+  ${bpMaxSM} {
+    .info {
+      padding: 0;
+    }
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    ${bpMaxSM} {
+      align-items: center;
+    }
+    width: 100%;
+    margin-bottom: 50px;
+    flex-direction: column;
+    :nth-of-type(odd) {
+      flex-direction: column;
+      .info {
+        padding: 0;
+      }
+    }
+    .image {
+      margin-bottom: 30px;
+      ${bpMaxMD} {
+        margin-top: 20px;
+      }
+    }
+  }
+  img {
+    display: block;
+    margin: 0;
+    width: 100%;
+    max-width: 320px;
+    height: auto;
+  }
+  .image {
+    ${bpMaxMD} {
+      margin-top: 30px;
+    }
+    overflow: none;
+    box-shadow: 0 15px 80px -20px rgba(0, 0, 0, 0.15);
+    transition: all 250ms ease-in-out;
+    border-radius: 5px;
+    overflow: hidden;
+    background: white;
+    :hover,
+    :focus {
+      box-shadow: 0 10px 50px -20px rgba(0, 0, 0, 0.35);
+      transition: all 250ms ease-in-out;
+      outline: none;
+    }
+  }
+  .button {
+    cursor: pointer;
+    > span > p {
+      padding: 13px 20px;
+      background: white;
+      color: black;
+      margin: 0;
+      margin-top: 15px;
+      box-shadow: 0 5px 30px -10px rgba(0, 0, 0, 0.15);
+      border-radius: 3px;
+      strong {
+        padding-left: 20px;
+        text-decoration: none;
+      }
+    }
+  }
+  p {
+    margin-bottom: 20px;
+    ${bpMaxMD} {
+      margin-bottom: 20px;
+    }
+  }
+`
+
 const Item = ({
   title,
   description,
@@ -16,7 +137,12 @@ const Item = ({
 }) => {
   return (
     <ItemBox>
-      <a href="#">
+      <a
+        href="#"
+        css={css`
+          flex-shrink: 0;
+        `}
+      >
         <div className="image">
           <img src={thumb} alt={title} />
         </div>
@@ -82,120 +208,3 @@ const Item = ({
 }
 
 export default Item
-
-const ItemBox = styled.div`
-  margin-bottom: 100px;
-  display: flex;
-  align-items: center;
-  ${bpMaxMD} {
-    align-items: flex-start;
-  }
-  ${bpMaxSM} {
-    align-items: center;
-  }
-  justify-content: center;
-  flex-direction: row;
-  width: 100%;
-  max-width: 920px;
-  :nth-of-type(odd) {
-    flex-direction: row-reverse;
-    .info {
-      padding-left: 0px;
-      padding-right: 80px;
-      ${bpMaxMD} {
-        padding-right: 40px;
-      }
-    }
-  }
-  .info {
-    padding-left: 80px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    ${bpMaxMD} {
-      padding-left: 40px;
-    }
-  }
-  ${bpMaxSM} {
-    .info {
-      padding: 0;
-    }
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    ${bpMaxSM} {
-      align-items: center;
-    }
-    width: 100%;
-    margin-bottom: 50px;
-    flex-direction: column;
-    :nth-of-type(odd) {
-      flex-direction: column;
-      .info {
-        padding: 0;
-      }
-    }
-    .image {
-      margin-bottom: 30px;
-      ${bpMaxMD} {
-        margin-top: 20px;
-      }
-    }
-  }
-  &img {
-    width: 100%;
-    max-width: 320px;
-    height: auto;
-  }
-  .image {
-    ${bpMaxMD} {
-      margin-top: 30px;
-    }
-    overflow: none;
-    box-shadow: 0 15px 80px -20px rgba(0, 0, 0, 0.15);
-    transition: all 250ms ease-in-out;
-    border-radius: 5px;
-    overflow: hidden;
-    background: white;
-    :hover,
-    :focus {
-      box-shadow: 0 10px 50px -20px rgba(0, 0, 0, 0.35);
-      transition: all 250ms ease-in-out;
-      outline: none;
-    }
-  }
-  .button {
-    cursor: pointer;
-    > span > p {
-      padding: 13px 20px;
-      background: white;
-      color: black;
-      margin: 0;
-      margin-top: 15px;
-      box-shadow: 0 5px 30px -10px rgba(0, 0, 0, 0.15);
-      border-radius: 3px;
-      strong {
-        padding-left: 20px;
-        text-decoration: none;
-      }
-    }
-  }
-  p {
-    margin-bottom: 20px;
-    ${bpMaxMD} {
-      margin-bottom: 20px;
-    }
-  }
-`
-
-const ItemInfo = styled.div`
-  width: 100%;
-  opacity: 0.9;
-  font-size: 17px;
-  color: #090909;
-  letter-spacing: 0;
-  line-height: 26px;
-  ${bpMaxSM} {
-    width: auto;
-  }
-`
