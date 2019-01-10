69cb7e41b371556f84f3d523633e1badb89a6ac4
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-01-10 16:45:33 +0200

    improve Container

--- src/components/markupHelpers/Container.jsx
+++ src/components/markupHelpers/Container.jsx
@@ -3,16 +3,16 @@ import { css } from '@emotion/core'
 import { bpMaxSM } from '../../lib/breakpoints'
 
 const Container = props => {
-  const { maxWidth = 1142, noPadding = false, ...restProps } = props
+  const { maxWidth = 900, noPadding = false, ...restProps } = props
   return (
     <div
       css={css`
         width: 100%;
         margin: 0 auto;
-        max-width: ${maxWidth + 80}px;
+        max-width: ${maxWidth + (noPadding ? 0 : 80)}px;
         padding: 0 ${noPadding ? 0 : '40px'};
         ${bpMaxSM} {
-          max-width: ${maxWidth + 40}px;
+          max-width: ${maxWidth + (noPadding ? 0 : 80)}px;
           padding: 0 ${noPadding ? 0 : '20px'};
         }
       `}
