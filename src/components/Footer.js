1a3721e2ecc5610e5b31165458c5f0e53c600f37
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-01-10 17:39:27 +0200

    subscribe form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,71 @@ import Logo from './Logo'
 
 import categories from '../data/categories'
 
+const SubmitBtn = props => {
+  const { children, ...restProps } = props
+  return (
+    <button
+      {...restProps}
+      css={css`
+        font-size: 16px;
+        line-height: 1.25;
+        padding: 12px 16px;
+        background: black;
+        color: white;
+        margin: 0;
+        box-shadow: 0 5px 30px -10px rgba(0, 0, 0, 0.15);
+        border-radius: 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        cursor: pointer;
+        border: none;
+        height: 50px;
+      `}
+    >
+      <span>{children}</span>
+      <span
+        css={css`
+          margin-left: 20px;
+        `}
+      >
+        →
+      </span>
+    </button>
+  )
+}
+
+const InputWrapperStyles = css`
+  width: 32.5%;
+  flex-shrink: 0;
+  ${bpMaxSM} {
+    width: 100%;
+    &:not(:first-child) {
+      margin-top: 20px;
+    }
+  }
+`
+const InputStyles = css`
+  width: 100%;
+  display: block;
+  border: 1px solid #d7d7d7;
+  box-shadow: none;
+  padding: 12px 16px;
+  height: 50px;
+  transition: 100ms;
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.4);
+    transition: 100ms;
+  }
+  &:focus {
+    outline: none;
+    &::placeholder {
+      color: transparent;
+    }
+  }
+`
+
 const Footer = () => (
   <footer
     css={css`
@@ -16,9 +81,6 @@ const Footer = () => (
       margin-top: 70px;
       background: #fafafa;
       flex-shrink: 0;
-      a {
-        color: inherit;
-      }
       h4 {
         font-size: 20px;
         font-family: ${fonts.bold}, sans-serif;
@@ -26,32 +88,77 @@ const Footer = () => (
     `}
   >
     <Container
-      maxWidth={1080}
-      noPadding
+      maxWidth={1000}
       css={css`
         background: white;
         box-shadow: 0 10px 50px -10px rgba(0, 0, 0, 0.05);
         border-radius: 5px;
+        padding-top: 80px;
+        padding-bottom: 80px;
+        ${bpMaxSM} {
+          padding-top: 40px;
+          padding-bottom: 40px;
+        }
       `}
     >
       <Container maxWidth={900}>
+        <div>
+          <h4>Subscribe to get our latest content by email</h4>
+          <div
+            css={css`
+              display: flex;
+              justify-content: space-between;
+              ${bpMaxSM} {
+                flex-direction: column;
+                max-width: 400px;
+                margin: 0 auto;
+              }
+            `}
+          >
+            <div css={InputWrapperStyles}>
+              <input
+                type="text"
+                placeholder="Your first name"
+                css={InputStyles}
+              />
+            </div>
+            <div css={InputWrapperStyles}>
+              <input
+                type="text"
+                placeholder="Your email address"
+                css={InputStyles}
+              />
+            </div>
+            <div css={InputWrapperStyles}>
+              <SubmitBtn>Subscribe</SubmitBtn>
+              <div
+                css={css`
+                  opacity: 0.6;
+                  color: #090909;
+                  font-size: 12px;
+                  line-height: 1.25;
+                  margin-top: 20px;
+                `}
+              >
+                We won't send you spam. Unsubscribe at any time.
+              </div>
+            </div>
+          </div>
+        </div>
         <div
           css={css`
             display: flex;
             align-items: flex-start;
-            padding: 70px 0;
+            margin-top: 80px;
+            a {
+              color: #090909;
+            }
           `}
         >
           <div>
-            <Link to="/#trainings">
-              <h4
-                css={css`
-                  margin-top: 10px;
-                `}
-              >
-                Trainings
-              </h4>
-            </Link>
+            <h4>
+              <Link to="/#trainings">Trainings</Link>
+            </h4>
             {categories.map(category => (
               <div
                 key={category.title}
