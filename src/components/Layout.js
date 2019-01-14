826ea685dd9628d2ccd2b69665b73a8f348965d9
Taylor Bell <taylorbell@gmail.com>
2019-01-14 11:12:31 -0700

    update query in layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -86,12 +86,12 @@ export default ({ site, frontmatter = {}, children, dark, noFooter }) => {
   const {
     title,
     description: siteDescription,
-    keywords: siteKeywords,
+    keywords: siteKeywords
   } = site.siteMetadata
 
   const {
     keywords: frontmatterKeywords,
-    description: frontmatterDescription,
+    description: frontmatterDescription
   } = frontmatter
 
   const keywords = (frontmatterKeywords || siteKeywords).join(', ')
@@ -111,10 +111,10 @@ export default ({ site, frontmatter = {}, children, dark, noFooter }) => {
           title={title}
           meta={[
             { name: 'description', content: description },
-            { name: 'keywords', content: keywords },
+            { name: 'keywords', content: keywords }
           ]}
         >
-          <html lang="en" />
+          <html lang='en' />
         </Helmet>
         <Global styles={globalStyles} />
         <Header dark={dark} />
@@ -148,7 +148,9 @@ export const pageQuery = graphql`
     siteMetadata {
       title
       description
-      author
+      author {
+        name
+      }
       keywords
     }
   }
