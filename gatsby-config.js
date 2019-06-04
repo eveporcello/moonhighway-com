a16d3b387d4df6f7a06f8dde2d0583531f703ba7
Vojta Holik <vojta@egghead.io>
2019-06-04 10:25:46 +0200

    tickets function

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 const mdxFeed = require('gatsby-mdx/feed')
+const proxy = require('http-proxy-middleware')
 
 module.exports = {
+  developMiddleware: app => {
+    app.use(
+      '/.netlify/functions/',
+      proxy({
+        target: 'http://localhost:9000',
+        pathRewrite: {
+          '/.netlify/functions/': '',
+        },
+      }),
+    )
+  },
   pathPrefix: '/',
   siteMetadata: {
     siteUrl: 'https://moonhighway.com/',
