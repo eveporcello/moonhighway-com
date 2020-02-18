bfa29445085fec51598db295c7a4da0f0a3e04d8
eveporcello <eve@moonhighway.com>
2020-02-18 08:28:19 -0800

    Adds to categories

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,15 +1,15 @@
-import javascript from '../images/javascript-group.svg'
-import react from '../images/react-group.svg'
-import graphql from '../images/graphql-group.svg'
+import javascript from '../images/javascript-group.svg';
+import react from '../images/react-group.svg';
+import graphql from '../images/graphql-group.svg';
 
 export default [
   {
     title: 'GraphQL Training',
     slug: '/graphql',
-    description: `GraphQL is a query language for your APIs, and GraphQL is everywhere. Whether you're a front-end developer trying to load data predictably into your UIs, a back-end developer looking to wrangle microservices, or a manager hoping to tap into the performance and productivity benefits of GraphQL, we have practical, hands-on courses that will fast-track your progress at creating production-ready services with GraphQL. Learn the latest from the authors of O'Reilly's [Learning GraphQL](http://shop.oreilly.com/product/0636920137269.do).`,
+    description: `GraphQL is a query language for your APIs, and GraphQL is everywhere. Whether you're a front-end developer trying to load data predictably into your UIs, a back-end developer looking to wrangle microservices, or a manager hoping to tap into the performance and productivity benefits of GraphQL, we have practical, hands-on courses that will fast-track your progress at creating production-ready services with GraphQL. We also offer [free GraphQL webinars once a month](https://moonhighway.com/graphql/everyone)!`,
     thumb: `${graphql}`,
     button: 'View More **→**',
-    tags: '',
+    tags: ''
   },
   {
     title: 'React Training',
@@ -17,7 +17,7 @@ export default [
     description: `Since its open-source release by Facebook in 2013, the React library has exploded in popularity due to its composable nature and declarative style. Many related tools and frameworks have emerged to supplement the features of React, and our classes introduce these with practicality in mind. With all of the recent React enhancements like Hooks and Suspense, our classes solidify the fundamentals while equipping students with the latest syntax and patterns.`,
     thumb: `${react}`,
     button: 'View More **→**',
-    tags: '',
+    tags: ''
   },
   {
     title: 'JavaScript & Node.js Training',
@@ -25,6 +25,6 @@ export default [
     description: `With JavaScript and Node.js training from Moon Highway, your team will learn the skills necessary to be successful at building full-stack JavaScript applications. Our beginner JavaScript courses emphasize the fundamentals of JavaScript. They are designed for developers, engineers, creatives and managers who have very little JavaScript experience. Our Node.js materials are designed to train you for the job. We focus heavily on unit testing, best practices, and real world scenarios.`,
     thumb: `${javascript}`,
     button: 'View More **→**',
-    tags: '',
-  },
-]
+    tags: ''
+  }
+];
