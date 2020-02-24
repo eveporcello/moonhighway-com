86b44ff75e8c869744b24823a4d00351fc337f02
Eve Porcello <eve@moonhighway.com>
2020-02-24 12:28:24 -0800

    Update index.js

--- src/pages/graphql/everyone/index.js
+++ src/pages/graphql/everyone/index.js
@@ -77,7 +77,7 @@ export default function GraphQLPage() {
             </li>
             <li>
               <a href="https://zoom.us/webinar/register/WN_ChjHRwgUTGGZSoG_SIQhhA">
-                April 3, 2020
+                April 13, 2020
               </a>
             </li>
           </ul>
